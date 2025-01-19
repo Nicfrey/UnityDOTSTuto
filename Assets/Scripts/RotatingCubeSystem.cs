571a6b747f98a1ed6dade2a161d480b9b886d760
Nicfrey <nicolas.fer9@gmail.com>
2025-01-19 17:20:01 +0100

    feat: job and burst

--- Assets/Scripts/RotatingCubeSystem.cs
+++ Assets/Scripts/RotatingCubeSystem.cs
@@ -1,3 +1,4 @@
+using Unity.Burst;
 using Unity.Entities;
 using Unity.Transforms;
 using UnityEngine;
@@ -6,15 +7,36 @@ public partial struct RotatingCubeSystem : ISystem // This is running on the mai
 {
     // has three different functions : OnCreate, OnUpdate and OnDestroy
 
+    [BurstCompile] // this can be added in the struct itself, but we only need to add it on the methods
     public void OnUpdate(ref SystemState state)
     {
-        // RefRW is for Read-Write
-        // RefRO is for Read-Only
-        // Goes to every entity that have those components
-        foreach (var (localTransform, rotateSpeed) in SystemAPI.Query<RefRW<LocalTransform>,RefRO<RotateSpeed>>())
+        /*
+            // RefRW is for Read-Write
+            // RefRO is for Read-Only
+            // Goes to every entity that have those components
+            foreach (var (localTransform, rotateSpeed) in SystemAPI.Query<RefRW<LocalTransform>,RefRO<RotateSpeed>>())
+            {
+                // Time.deltaTime is not working with entities
+                localTransform.ValueRW = localTransform.ValueRW.RotateY(rotateSpeed.ValueRO.value * SystemAPI.Time.DeltaTime);
+            }
+        */
+
+        RotatingCubeJob rotatingCubeJob = new RotatingCubeJob
         {
-            // Time.deltaTime is not working with entities
-            localTransform.ValueRW = localTransform.ValueRW.RotateY(rotateSpeed.ValueRO.value * SystemAPI.Time.DeltaTime);
+            deltaTime = SystemAPI.Time.DeltaTime
+        };
+        rotatingCubeJob.Schedule();
+    }
+
+    [BurstCompile]
+    // This allows us to make this rotation on multiple thread
+    public partial struct RotatingCubeJob : IJobEntity
+    {
+        // We can't access the SystemAPI.Time.DeltaTime so we need to set up ourselve during update
+        public float deltaTime;
+        public void Execute(ref LocalTransform localTransform, in RotateSpeed rotateSpeed)
+        {
+            localTransform = localTransform.RotateY(rotateSpeed.value * deltaTime);
         }
     }
 
