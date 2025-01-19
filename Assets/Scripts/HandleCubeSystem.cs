bc57b200ef719a06879d6c6b14cf26b8b1abea73
Nicfrey <nicolas.fer9@gmail.com>
2025-01-19 17:44:12 +0100

    feat: aspect

--- Assets/Scripts/HandleCubeSystem.cs
+++ Assets/Scripts/HandleCubeSystem.cs
@@ -5,11 +5,10 @@ public partial struct HandleCubeSystem : ISystem
 {
     public void OnUpdate(ref SystemState state)
     {
-        foreach (var (localTransform,rotateSpeed, movement) in 
-                 SystemAPI.Query<RefRW<LocalTransform>, RefRO<RotateSpeed>, RefRO<Movement>>().WithAll<RotatingCube>())
+        foreach (RotatingMovingCubeAspect rotatingMovingCubeAspect in 
+                 SystemAPI.Query<RotatingMovingCubeAspect>())
         {
-            localTransform.ValueRW = localTransform.ValueRO.RotateY(rotateSpeed.ValueRO.value * SystemAPI.Time.DeltaTime);
-            localTransform.ValueRW = localTransform.ValueRO.Translate(movement.ValueRO.movementVector * SystemAPI.Time.DeltaTime);
+            rotatingMovingCubeAspect.MoveAndRotate(SystemAPI.Time.DeltaTime);
         }
     }
 }
